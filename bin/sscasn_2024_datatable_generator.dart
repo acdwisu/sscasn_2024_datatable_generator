002acffd3ad3c65076d157debf6288bf02b22cb7
acdwisu <achmaddwisuriyanto@gmail.com>
2024-08-27 23:43:53 +0700

    initiate project

---
+++ bin/sscasn_2024_datatable_generator.dart
@@ -0,0 +1,5 @@
+import 'package:sscasn_2024_datatable_generator/sscasn_2024_datatable_generator.dart' as sscasn_2024_datatable_generator;
+
+void main(List<String> arguments) {
+  print('Hello world: ${sscasn_2024_datatable_generator.calculate()}!');
+}
---
+++ test/sscasn_2024_datatable_generator_test.dart
@@ -0,0 +1,8 @@
+import 'package:sscasn_2024_datatable_generator/sscasn_2024_datatable_generator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
